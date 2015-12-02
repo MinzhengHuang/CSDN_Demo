4772d84907fc3993721b87ce4ee60beb9fd354e6
黄敏争 <ahtchmz@163.com>
2015-12-02 15:02:30 +0800

    新建项目

---
+++ csdn/src/com/csdn/CSDNApplication.java
@@ -0,0 +1,16 @@
+package com.csdn;
+
+import android.app.Application;
+
+/**
+ * Created by huangminzheng on 15/11/26.
+ */
+public class CSDNApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+}
