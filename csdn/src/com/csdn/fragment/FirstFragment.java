96435ade004921f67cb5152ac28e58042d2b190b
黄敏争 <ahtchmz@163.com>
2016-01-15 11:45:04 +0800

    format add Risenumber

--- csdn/src/com/csdn/fragment/FirstFragment.java
+++ csdn/src/com/csdn/fragment/FirstFragment.java
@@ -9,29 +9,34 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
 import com.csdn.R;
+import com.csdn.activity.RiseNumberActivity;
 import com.csdn.touchevent.TouchEventActivity;
 
-public class FirstFragment extends Fragment implements OnClickListener{
+public class FirstFragment extends Fragment implements OnClickListener {
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_first, container, false);
-		view.findViewById(R.id.btn_TouchEvent).setOnClickListener(this);
-		return view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        view.findViewById(R.id.btn_TouchEvent).setOnClickListener(this);
+        view.findViewById(R.id.btn_RiseNumber).setOnClickListener(this);
+        return view;
 
-	}
+    }
 
-	@Override
-	public void onClick(View v) {
-		Intent intent=null;
-		switch (v.getId()) {
-		case R.id.btn_TouchEvent:
-			intent=new Intent(getActivity(), TouchEventActivity.class);
-			break;
-		default:
-			break;
-		}
-		startActivity(intent);
-	}
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.btn_TouchEvent:
+                intent = new Intent(getActivity(), TouchEventActivity.class);
+                break;
+            case R.id.btn_RiseNumber:
+                intent = new Intent(getActivity(), RiseNumberActivity.class);
+                break;
+            default:
+                break;
+        }
+        startActivity(intent);
+    }
 }
