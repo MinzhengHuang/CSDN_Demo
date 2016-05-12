9db39c5a1d6ba733c7afb1f956346ad15b7b6196
黄敏争 <huangminzheng@hmz-Mac.local>
2016-05-12 15:32:56 +0800

    format

--- csdn/src/com/csdn/fragment/SecondFragment.java
+++ csdn/src/com/csdn/fragment/SecondFragment.java
@@ -10,24 +10,24 @@ import android.view.ViewGroup;
 
 import com.csdn.R;
 
-public class SecondFragment extends Fragment implements OnClickListener{
+public class SecondFragment extends Fragment implements OnClickListener {
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_second, container, false);
-		return view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        return view;
 
-	}
+    }
 
-	@Override
-	public void onClick(View v) {
-		Intent intent=null;
-		switch (v.getId()) {
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
 
-		default:
-			break;
-		}
-		startActivity(intent);
-	}
+            default:
+                break;
+        }
+        startActivity(intent);
+    }
 }
